974c8718da9bb3b03771625677d11e168e026274
ss251 <ss251@uw.edu>
2024-08-06 00:28:53 +0530

    chore: update header verbiage

--- components/Header.tsx
+++ components/Header.tsx
@@ -115,7 +115,7 @@ export function Header() {
               className="hidden md:block bg-primary text-white hover:bg-primary/90"
               onClick={() => setIsCalendlyOpen(true)}
             >
-              Book Now
+              Book walk-through
             </Button>
             <button
               className="md:hidden text-gray-500 hover:text-gray-700"
